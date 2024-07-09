d433fd479755662454fcc561485c538d50456ffd
Tao Zhang <forrest_zhang@163.com>
2024-07-09 17:04:08 +0800

    new add

---
+++ 2_prompt_templates/2_prompt_template_with_chat_model.py
@@ -0,0 +1,29 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+from langchain_community.chat_models import ChatOllama
+
+model = ChatOllama(model='llama3')
+
+# Part 1, Prompt with one placeholder
+template = "Tell me a joke about {topic}."
+
+prompt_template = ChatPromptTemplate.from_template(template=template)
+
+print("---------------Prompt from Template--------------")
+prompt = prompt_template.invoke({"topic": "chicken"})
+print(prompt)
+result = model.invoke(prompt)
+print(result.content)
+
+
+# Part 3, Prompt with System and Human Message (Using Tuples)
+messages =[
+("system","You are a comedian who tells jokes about {topic}."),
+("human","Tell me {joke_count} jokes.")
+]
+prompt_template_messages = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template_messages.invoke({"topic": "lawyers", "joke_count": 3})
+print("---------------Prompt with System and Human Message (Tuple)--------------")
+print(prompt)
+result = model.invoke(prompt)
+print(result.content)
