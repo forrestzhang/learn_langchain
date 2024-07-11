008663859126b80c45fbd7cdf9a101ed748552f5
Tao Zhang <forrest_zhang@163.com>
2024-07-11 11:17:37 +0800

    new add

---
+++ 3_chains/5_chains_branching.py
@@ -0,0 +1,84 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableBranch
+from langchain_community.chat_models import ChatOllama
+
+
+model = ChatOllama(model='llama3')
+
+positive_feedback_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a help assisant."),
+        ("human", "Generate a thank you note for this positive feedback: {feedback}.")
+    ]
+)
+
+negative_feedback_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a help assisant."),
+        ("human", "Generate a negative note for this negative feedback: {feedback}.")
+    ]
+)
+
+neutral_feedback_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a help assisant."),
+        ("human", "Generate a neutral note for this netural feedback: {feedback}.")
+    ]
+)
+
+escalate_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a help assisant."),
+        ("human", "Generate a message to escalate this feedback to a human agent: {feedback}.")
+    ]
+)
+
+classification_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a help assisant."),
+        ("human", "Classify the sentiment of this feedback as positive, negative, netral or escalate: {feedback}.")
+    ]
+)
+
+
+branches = RunnableBranch(
+    (
+        lambda x: "positive" in x,
+        positive_feedback_template | model | StrOutputParser()
+    ),
+    (
+        lambda x: "negative" in x,
+        negative_feedback_template | model | StrOutputParser()
+    ),
+    (
+        lambda x: "neutral" in x,
+        neutral_feedback_template | model | StrOutputParser()
+    ),
+    escalate_template | model | StrOutputParser() # Default branch
+    
+    
+)
+
+
+classification_chain = (
+    classification_template
+    | model
+    | StrOutputParser()
+    
+)   
+
+chain = classification_chain | branches
+
+# Good Review
+# review = "The product is excellent. I really enjoyed using it and found it very helpful."
+# Bad Review
+# review = "This product is terrible. It broke after just one use and the quality is very poor."
+# Neutral Review
+# review = "The product is okay. It works as expected but nothing special."
+# Default
+review = "I'm not sure about this product. Can you tell me more about this?"
+
+result = chain.invoke({"feedback": review})
+
+print(result)
---
+++ 4_RAG/1_a_rag_basics.py
@@ -0,0 +1,43 @@
+import os
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.join(current_dir, "books", "odyssey.txt")
+persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+
+
+# check chroma db exists
+if not os.path.exists(persistent_directory):
+    print("Presistent directory does not exits. Creating Chroma DB")
+    
+    #os.makedirs(persistent_directory)
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File {file_path} not found")
+    
+    # Read the text from file
+    loader = TextLoader(file_path)
+    documents = loader.load()
+
+    # Split the document into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    docs = text_splitter.split_documents(documents)
+
+    print("\n ----Document Chunks Information----")
+    print(f"Number of documents: {len(docs)}")
+    print(f"Sample chunk:\n{docs[0].page_content}\n")
+
+    # Create a Chroma Vector Store
+
+    embedding = OllamaEmbeddings(model='nomic-embed-text')
+
+    db = Chroma.from_documents(documents=docs, embedding=embedding, persist_directory=persistent_directory)
+    print("-------Chroma DB created-------")
+
+else:
+    print("Chroma DB already exists")
+ 
