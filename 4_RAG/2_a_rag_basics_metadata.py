ddd37aed2c65a9f1b2d6b6863d9318c523305009
Tao Zhang <forrest_zhang@163.com>
2024-07-12 21:53:34 +0800

    change to huggingface embeddings

--- 4_RAG/2_a_rag_basics_metadata.py
+++ 4_RAG/2_a_rag_basics_metadata.py
@@ -4,6 +4,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 books_dir = os.path.join(current_dir, "books")
@@ -43,7 +44,16 @@ if not os.path.exists(persistent_directory):
     print("\n ----Document Chunks Information----")
     print(f"Number of documents: {len(docs)}")
 
-    embeddings = OllamaEmbeddings(model='nomic-embed-text')
+    # embeddings = OllamaEmbeddings(model='nomic-embed-text',
+    #                               base_url="http://gpuserver:11434",
+    #                               show_progress=True)
+    model_kwargs = {'device': 'cuda', 'trust_remote_code': True}
+    encode_kwargs = {'normalize_embeddings': False}
+    model_name = "BAAI/bge-base-en-v1.5"
+    embeddings = HuggingFaceEmbeddings(model_name=model_name, 
+                                       model_kwargs=model_kwargs,
+                                        encode_kwargs=encode_kwargs,
+                                       show_progress=True)
 
     db = Chroma.from_documents(documents=docs, 
                                embedding=embeddings, 
